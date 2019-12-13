b29aaf46742439784e4f98797d333db6dff04a01
Jonathan Scott <jonathan.scott@gmail.com>
2019-12-12 21:04:52 -0800

    partial solution on making change. needs caching

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,26 +2,23 @@
 
 import sys
 
-def rock_paper_scissors(n, cache={}):
-  # create a list of options
+def rock_paper_scissors(n):
   final = []
   options = [['rock'], ['paper'], ['scissors']]
-  
   # recursive calls 'n' times over options generating all permutations
   def list_gen(n, arr=[]):
     if n == 0: return final.append(arr)
-    # add options to val 'n' times
+    # add options 'i' to arr 'n' times
     for i in options:
       list_gen(n-1, arr + i)
-    return final
-  # if not 'final' in cache:
-  #   cache['final'] = list_gen(n) + list_gen(n) + list_gen(n)
+    # return final
   list_gen(n)
   return final
-print(rock_paper_scissors(4))
-# if __name__ == "__main__":
-#   if len(sys.argv) > 1:
-#     num_plays = int(sys.argv[1])
-#     print(rock_paper_scissors(num_plays))
-#   else:
-#     print('Usage: rps.py [num_plays]')
+print(rock_paper_scissors(3))
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_plays = int(sys.argv[1])
+    print(rock_paper_scissors(num_plays))
+  else:
+    print('Usage: rps.py [num_plays]')
